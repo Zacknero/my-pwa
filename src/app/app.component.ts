101916bddd1878a4f34afd6ba996f72802a9ffab
Zacknero <nerodevilman@live.it>
2018-10-19 21:33:15 +0200

    -update npm -update packages -added navigation event for insert the spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {SwUpdate} from '@angular/service-worker';
 import {MatSnackBar} from '@angular/material';
+import {Event, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,20 @@ import {MatSnackBar} from '@angular/material';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private swUpdate: SwUpdate, private snackBar: MatSnackBar) {
+  constructor(private swUpdate: SwUpdate, private snackBar: MatSnackBar, private router: Router) {
+    this.router.events.subscribe((routerEvent: Event) => {
+
+      if (routerEvent instanceof NavigationStart) {
+        //this.showLoadingIndicator = true;
+        console.log('Rout start');
+      }
+
+      if (routerEvent instanceof NavigationEnd || routerEvent instanceof NavigationError || routerEvent instanceof NavigationCancel) {
+        ///this.showLoadingIndicator = false;
+        console.log('Rout end');
+      }
+
+    });
   }
 
   ngOnInit() {
